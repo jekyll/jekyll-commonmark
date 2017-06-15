1900d8235ad052d269a7ab7233641d06517f1860
Yuki Izumi <yuki@kivikakk.ee>
2017-06-15 13:34:30 +1000

    Support CommonMark extensions

--- lib/jekyll-commonmark.rb
+++ lib/jekyll-commonmark.rb
@@ -12,9 +12,22 @@ module Jekyll
           @options = [:DEFAULT]
         else
           @options.reject! do |e|
-            unless CommonMarker::Config::Parse.keys.include? e.to_sym
+            unless CommonMarker::Config::Parse.keys.include? e
               Jekyll.logger.warn "CommonMark:", "#{e} is not a valid option"
               Jekyll.logger.info "Valid options:", CommonMarker::Config::Parse.keys.join(", ")
+              true
+            end
+          end
+        end
+        begin
+          @extensions = config['commonmark']['extensions'].collect { |e| e.to_sym }
+        rescue NoMethodError
+          @extensions = []
+        else
+          @extensions.reject! do |e|
+            unless CommonMarker.extensions.include? e.to_s
+              Jekyll.logger.warn "CommonMark:", "#{e} is not a valid extension"
+              Jekyll.logger.info "Valid extensions:", CommonMarker.extensions.join(", ")
               true
             end
           end
@@ -22,7 +35,7 @@ module Jekyll
       end
 
       def convert(content)
-        CommonMarker.render_doc(content, @options).to_html
+        CommonMarker.render_doc(content, @options, @extensions).to_html
       end
     end
   end
