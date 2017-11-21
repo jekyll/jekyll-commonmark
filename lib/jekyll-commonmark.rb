2ea2d2e7222cdcc73f5f4fc990360c4e8fb9fbf2
jekyllbot <jekyllbot@users.noreply.github.com>
2017-11-21 10:11:13 -0500

    Only set options once (#17)
    Merge pull request 17

--- lib/jekyll-commonmark.rb
+++ lib/jekyll-commonmark.rb
@@ -6,12 +6,12 @@ module Jekyll
       def initialize(config)
         Jekyll::External.require_with_graceful_fail "commonmarker"
         begin
-          @options = config["commonmark"]["options"].collect { |e| e.upcase.to_sym }
+          options = config["commonmark"]["options"].collect { |e| e.upcase.to_sym }
         rescue NoMethodError
-          @options = []
+          options = []
         else
           valid_opts = Set.new(CommonMarker::Config::Parse.keys + CommonMarker::Config::Render.keys)
-          @options.reject! do |e|
+          options.reject! do |e|
             next if valid_opts.include? e
             Jekyll.logger.warn "CommonMark:", "#{e} is not a valid option"
             Jekyll.logger.info "Valid options:", valid_opts.to_a.join(", ")
@@ -30,14 +30,18 @@ module Jekyll
             true
           end
         end
+
+        options_set = Set.new(options).freeze
+        @parse_options = (options_set & CommonMarker::Config::Parse.keys).to_a
+        @render_options = (options_set & CommonMarker::Config::Render.keys).to_a
+
+        @parse_options = :DEFAULT if @parse_options.empty?
+        @render_options = :DEFAULT if @render_options.empty?
       end
 
       def convert(content)
-        parse_options = (Set.new(@options) & CommonMarker::Config::Parse.keys).to_a
-        render_options = (Set.new(@options) & CommonMarker::Config::Render.keys).to_a
-        parse_options = :DEFAULT if parse_options.empty?
-        render_options = :DEFAULT if render_options.empty?
-        CommonMarker.render_doc(content, parse_options, @extensions).to_html(render_options, @extensions)
+        CommonMarker.render_doc(content, @parse_options, @extensions)
+          .to_html(@render_options, @extensions)
       end
     end
   end
