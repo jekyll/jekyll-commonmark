7ed18a743c3e68a8dd0ab3dd45562722902ee247
Ashwin Maroli <ashmaroli@users.noreply.github.com>
2020-06-22 20:06:48 +0530

    Test rendering with invalid configuration (#27)
    Merge pull request 27

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,4 +9,15 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = "random"
+
+  def capture_stdout(level = :debug)
+    buffer = StringIO.new
+    Jekyll.logger = Logger.new(buffer)
+    Jekyll.logger.log_level = level
+    result = yield
+    buffer.rewind
+    [result, buffer.string.to_s]
+  ensure
+    Jekyll.logger = Logger.new(StringIO.new, :error)
+  end
 end
